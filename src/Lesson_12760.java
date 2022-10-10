14c6f042648148602ccb58fe72d6c63a05e56180
KirillNN <wmforum@bk.ru>
2022-10-10 18:09:50 +0300

    Add lesson

--- src/Lesson_12760.java
+++ src/Lesson_12760.java
@@ -1,11 +1,39 @@
+import java.lang.Integer;
+
 public class Lesson_12760 {
     public static void main(String[] args) {
-        System.out.println(charExpression(32));
+//        System.out.println(charExpression(32));
+        System.out.println(isPowerOfTwo(0));
+        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwo(-2));
+        System.out.println(isPowerOfTwo(32));
+        System.out.println(isPowerOfTwo(128));
+        System.out.println(isPowerOfTwo(-1024));
     }
 
     public static char charExpression(int a) {
         return (char) ('\\' + a);
     }
 
-
+    /**
+     * Checks if given <code>value</code> is a power of two.
+     *
+     * @param value any number
+     * @return <code>true</code> when <code>value</code> is power of two, <code>false</code> otherwise
+     */
+    public static boolean isPowerOfTwo(int value) {
+        String xxx = Integer.toString(value);
+//        System.out.println(xxx);
+//        System.out.println("value = " + value);
+//        value = Math.abs(value);
+//        int x = Integer.bitCount(Math.abs(value));
+//        System.out.println(x);
+//        System.out.println("value (abs) = " + value);
+//        String x = Integer.toBinaryString(value);
+//        System.out.println("value to string  = " + x);
+//        System.out.println(x.bitCount());
+//        char[] chars = x.toCharArray()
+//        System.out.println(chars);
+        return Integer.bitCount(Math.abs(value)) == 1;
+    }
 }
---
+++ src/Lesson_12761.java
@@ -0,0 +1,13 @@
+public class Lesson_12761 {
+    public static void main(String[] args) {
+        step8();
+    }
+
+    public static void step8() {
+        System.out.println('A' + '1' + "2");
+        System.out.println("A" + 12);
+        System.out.println("A" + ('\t' + '\u0003'));
+        System.out.println('A' + "12");
+
+    }
+}
