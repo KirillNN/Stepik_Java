1f91d2592e6a3e6366d0beadc04ae4ea031b5d1f
Smrinov <wmforum@bk.ru>
2022-10-12 01:20:53 +0300

    Modify ZywOo

--- src/ZywOo.java
+++ src/ZywOo.java
@@ -1,19 +1,29 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.*;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class ZywOo {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(multipleOfIndex(new int[]{22, -6, 32, 82, 9, 25})));
+        System.out.println(Arrays.toString(multipleOfIndex1(new int[]{22, -6, 32, 82, 9, 25})));
     }
 
     public static int[] multipleOfIndex(int[] array) {
-        List<Integer> list = new ArrayList<Integer>();
-        Collections.addAll(list, array);
-//        numList.add(1);
-//        numList.addAll(array);
-        return new int[]{0};
-//        numList.removeIf(i -> (i>10&& i<20));
+//        List<Integer> list = Arrays.stream(array).boxed().toList();
+        List<Integer> intList = new ArrayList<>(array.length);
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] % i == 0) {
+                intList.add(array[i]);
+            }
+        }
+        int[] res = new int[intList.size()];
+        for (int i = 0; i < res.length; i++)
+            res[i] = intList.get(i);
+        return res;
+    }
+
+    public static int[] multipleOfIndex1(int[] a) {
+        return IntStream.range(1, a.length).filter(i -> a[i] % i == 0).map(i -> a[i]).toArray();
     }
 }
