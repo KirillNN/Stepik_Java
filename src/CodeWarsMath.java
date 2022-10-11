61d4a6a3a486dab24b83683163a5b42f43a3e207
KirillNN <wmforum@bk.ru>
2022-10-11 13:27:55 +0300

    Add

--- src/CodeWarsMath.java
+++ src/CodeWarsMath.java
@@ -1,8 +1,20 @@
 public class CodeWarsMath {
     public static void main(String[] args) {
+        System.out.println(nearestSq(1));
         System.out.println(nearestSq(2));
+        System.out.println(nearestSq(10));
+        System.out.println(nearestSq(111));
+        System.out.println(nearestSq(9999));
     }
-    public static int nearestSq(final int n){
-        return 2;
+
+    public static int nearestSq(final int n) {
+        int n_sqrt = (int) Math.sqrt(n);
+        int left = n_sqrt * n_sqrt; // left value
+        int right = (n_sqrt + 1) * (n_sqrt + 1); // right value
+        if (Math.abs(left - n) < Math.abs(right - n)) {
+            return left;
+        } else {
+            return right;
+        }
     }
 }
